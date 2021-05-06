d3a5f298bd5faeae4df26991e8857f3f8d388b21
Matt Barnes <barnesmd1@gmail.com>
2021-05-06 18:06:34 -0400

    spacing changes

--- 100 days/Projects/Day 3- Treasure Island.py
+++ 100 days/Projects/Day 3- Treasure Island.py
@@ -26,29 +26,29 @@ death lurks around every corner!")
 cross_roads = input("You arrive at a cross road. \
 Do you wish to go left or right?\n").lower()
 if cross_roads == "left":
-    lake = input("You come to a lake, which has an island in the middle.\
-Type 'wait' to wait for a boat, or 'swim' to swim across.\n").lower()
+    lake = input("You come to a lake, which has an island in the middle. \
+    Type 'wait' to wait for a boat, or 'swim' to swim across.\n").lower()
     if lake == "wait":
         door = input("You arrive at the island unharmed, \
-and find yourself in front of a house with three doors. \
-One door is red, one is yellow, and one is blue. \
-Which color do you choose?\n").lower()
+        and find yourself in front of a house with three doors. \
+        One door is red, one is yellow, and one is blue. \
+        Which color do you choose?\n").lower()
         if door == "red":
-            print("The door opens into the very pit of hell.\
+            print("The door opens into the very pit of hell. \
                 You are devoured by flames. Game Over.")
         elif door == "blue":
-            print("The door opens and an angry wizard casts magic at you.\
+            print("The door opens and an angry wizard casts magic at you. \
                 Your body is immediately frozen and shatters. Game Over.")
         elif door == "yellow":
-            print("The door opens and you see a small box,\
-                sitting on the floor. Opening it, you find riches beyond\
+            print("The door opens and you see a small box, \
+                sitting on the floor. Opening it, you find riches beyond \
                 your wildest dreams! You have won!")
         else:
             print("You die. Game over.")
     else:
-        print("You attempt to swim the lake, as you are quite fit.\
-        Midway to the island though you find that you never seem to get\
+        print("You attempt to swim the lake, as you are quite fit. \
+        Midway to the island though you find that you never seem to get \
         closer and end up drowning. Game Over.")
 else:
-    print("You find yourself more and more lost, and starve to death.\
+    print("You find yourself more and more lost, and starve to death. \
     Game over.")
